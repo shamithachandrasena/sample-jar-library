ba5a297c5b80b6359f7462f51d4a5fdb5cb62381
Shamitha Chandrasena <shamitha.chandrasena@mvv.se>
2020-04-25 10:28:05 +0530

    Initial commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+    private String name;
+    private String owner;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
